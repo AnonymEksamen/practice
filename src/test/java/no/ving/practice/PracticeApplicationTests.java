2d75732935b3e9b5cfc3c6f501c2e9a317ce81f7
anonym <anonymeksamen123@gmail.com>
2019-02-09 15:57:03 +0100

    fafa

--- src/test/java/no/ving/practice/PracticeApplicationTests.java
+++ src/test/java/no/ving/practice/PracticeApplicationTests.java
@@ -5,12 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PracticeApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void failingTest() {
+		boolean x = false;
+
+		assertEquals(true, x);
 	}
 
 }
